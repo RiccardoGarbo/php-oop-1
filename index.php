7f543b5f7c326101d4732fd55797e4450902e53c
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-24 11:36:22 +0100

    intantiated object , and print on screen the property

--- index.php
+++ index.php
@@ -15,14 +15,15 @@
   }
 
   public function infoMovie(){
-     echo "Titolo : {$this->title} <br> Genere : {$this->type} <br> Durata : {$this->duration} minuti <br> Anno: {$this->year}<br> Attore protagonista : {$this->actor}";
+     echo "Titolo : {$this->title} <br> Genere : {$this->type} <br> Durata : {$this->duration} minuti <br> Anno: {$this->year}<br> Attore protagonista : {$this->actor} <hr>";
   }
 
 }
+$movie1 = new Movie('Io sono leggenda', 'Azione/Shi-fi', 101 , 2007, 'Will Smith');
+$movie1->infoMovie();
 
-
-
-
+$movie2 = new Movie('The Wolf of Wall Street ', 'Giallo/Commedia', 180 , 2013, 'Leonado DiCaprio');
+$movie2->infoMovie();
 
 
 
