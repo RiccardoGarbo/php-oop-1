0c237edf4a85e92cfa5308ecaa2a2c3772cd3262
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-24 10:48:02 +0100

    add construct

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<php class Movie {
+<?php class Movie {
 
     public $title;
     public $type;
     public $duration;
     public $actor; 
+
+  public function __construct( $title ,$type,$duration,$actor) {
+    $this->title = $title;
+    $this->type = $type;
+    $this->duration= $duration;
+    $this->actor = $actor
+
+  };
 }
+
+
+
 <?
 
