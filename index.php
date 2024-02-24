2ef3a45e3194f1d77a7abfc12193a8473637e225
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-24 11:18:54 +0100

    add method

--- index.php
+++ index.php
@@ -3,18 +3,29 @@
     public $title;
     public $type;
     public $duration;
+    public $year;
     public $actor; 
 
-  public function __construct( $title ,$type,$duration,$actor) {
+  public function __construct( $title ,$type,$duration,$year, $actor) {
     $this->title = $title;
     $this->type = $type;
     $this->duration= $duration;
-    $this->actor = $actor
+    $this->year = $year;
+    $this->actor = $actor;
+  }
 
-  };
+  public function infoMovie(){
+     echo "Titolo : {$this->title} <br> Genere : {$this->type} <br> Durata : {$this->duration} minuti <br> Anno: {$this->year}<br> Attore protagonista : {$this->actor}";
+  }
+
 }
 
 
 
-<?
+
+
+
+
+
+
 
